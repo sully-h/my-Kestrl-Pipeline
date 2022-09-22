8f425806324d1465f966961f54180d75a0067d3c
sully-h <sulaymanhussain@hotmail.co.uk>
2022-09-22 12:26:12 +0100

    Increase cohesion in classes. Use a Extraction class from the internet and a Extraction class from the PDFs (for the tables)

--- main.py
+++ main.py
@@ -13,20 +13,26 @@ from parsr_getter import parsr_table_maker
 from pdf_URL_getter import get_URL_google, download_annual_report
 
 
-class StorePDFTables:
+class Extract:
     # TODO everything is inside the __init__ scope -> how should I pass around self.master_stocks between my functions?
     def __init__(self, path):
-        '''initialises the data and makes sure everything is there'''
-        self.trouble_some_pdfs = {} # for debugging the problem stock tickers
+        '''initialises the data from internet and makes sure everything is there'''
         # TODO if a list of paths is passed in, merge them (on which column?) OR save for elsewhere
         self.master_stocks = pd.read_csv(path).dropna()  # perhaps this should be read from s3
         self.total_revenue_tables_dict = {}  # don't need this since no camelot tables
 
-        self.check_is_revenue_col() # check if ["Total Revenue 20/21"] is a column and place it there
-        self.check_is_URL_col() # check if ["annual_report_URL"] is a column and place it there
-        self.check_is_PDF_path() # check if ["PDF_path"] is a column and place it there
+        self.check_is_revenue_col()  # check if ["Total Revenue 20/21"] is a column and place it there
+        self.check_is_URL_col()  # check if ["annual_report_URL"] is a column and place it there
+        self.check_is_PDF_path()  # check if ["PDF_path"] is a column and place it there
+    # TODO validate the data between stages properly and collect that data
+        self.trouble_some_pdfs = {}  # for catching and debugging the problem stock tickers
+
+    def catch_error(self):
+        '''Validate the data at extraction and catch errors for bad extractions'''
+        pass
 
     def check_is_revenue_col(self):
+        '''check if the Total Revenue 20/21 is present as a column and if not get it'''
         # yahoo revenue data doesn't always match the annual reports so needs work
         try:  # check if revenue as a column exists in the data
             self.master_stocks["Total Revenue 20/21"]
@@ -35,28 +41,45 @@ class StorePDFTables:
         except KeyError:  # if it doesn't get the data from yahoo
             self.master_stocks = main_revenue_getter(self.master_stocks,
                                                      "EOD_LSE_merged_filtered_with_URLS_PDF_paths_with_Total_Revenue.csv")
+            self.catch_error()
 
     def check_is_URL_col(self):
+        '''check if the annual_report_URL is present as a column and if not get it'''
         try:
             self.master_stocks["annual_report_URL"]
         except:
             self.master_stocks.apply(lambda row: get_URL_google(row, trouble_some_pdfs=self.trouble_some_pdfs), axis=1)
 
     def check_is_PDF_path(self):
-
-        """these PDFs will be in s3 and this method will need to discover them or put them there"""
+        '''check if the PDF_path is present as a column and if not get it'''
+        #    """these PDFs will be in s3 and this method will need to discover them or put them there"""
         try:
             self.master_stocks["PDF_path"]
         except KeyError:
-            self.master_stocks.apply(lambda row: download_annual_report(row, trouble_some_pdfs=self.trouble_some_pdfs), axis=1)
+            self.master_stocks.apply(lambda row: download_annual_report(row, trouble_some_pdfs=self.trouble_some_pdfs),
+                                     axis=1)
 
-    def get_csv_tables_from_pdf(self):
-        """produce CSV files for each table"""
+class TableScraper:
+    '''use extracted data (PDFs) to extract tables'''
+    def __init__(self, path):
+        self.master_stocks = Extract(path).master_stocks
+
+    def get_csv_tables_from_pdf_camelot(self):
+        '''use Camelot to parse PDF documents into CSV files'''
+        pass
+
+    def get_csv_tables_from_pdf_tabula(self):
+        '''use Tabula to parse PDF documents into CSV files'''
+        pass
+
+    def get_csv_tables_from_pdf_parsr(self):
+        """use Parsr(Docker Image) to produce CSV files for each table"""
         parsr_table_maker(self.master_stocks)
         # parsr_table_s3_uploader
 
-
+class Application:
+    pass
 
 if __name__ == '__main__':
-    TablePDF = StorePDFTables(
-            path="EOD_LSE_merged_filtered_with_URLS_PDF_paths.csv")
+    #TablePDF = Extract(path="EOD_LSE_merged_filtered_with_URLS_PDF_paths.csv")
+    table_scraper = TableScraper("EOD_LSE_merged_filtered_with_URLS_PDF_paths_with_Total_Revenue.csv")
